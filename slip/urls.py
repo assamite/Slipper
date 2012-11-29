f9bbf9306934fe780c83febe92dc12e59911dab3
Assamite <slinkola@cs.helsinki.fi>
2012-11-29 12:47:06 +0200

    Added URL_PREFIX in local_conf_clear and use this in root url conf (urls.py)

--- slip/urls.py
+++ slip/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from views import home, freudify
+from local_conf import URL_PREFIX
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,8 +9,8 @@ from views import home, freudify
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'slipper.views.home', name='home'),
-    url(r'^home', home, name = 'slip_home_url'),
-    url(r'^freudify/(?P<url>.*)$', freudify, name = "slip_freudify_url")
+    url(r'^%shome' % URL_PREFIX, home, name = 'slip_home_url'),
+    url(r'^%sfreudify/(?P<url>.*)$' % URL_PREFIX, freudify, name = "slip_freudify_url")
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

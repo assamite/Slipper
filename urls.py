9e7ddf16e03df6fcc396275ce824e060947d5bf7
Assamite <slinkola@cs.helsinki.fi>
2012-11-29 12:57:50 +0200

    Now it might actually work (check last commit)

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 import settings
-from local_conf import URL_PREFIX
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,8 +8,8 @@ from local_conf import URL_PREFIX
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'slipper.views.home', name='home'),
-    url(r'^%s' % URL_PREFIX, include('Slipper.slip.urls')),
-    url(r'^%smedia/(?P<path>.*)$' % URL_PREFIX, 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}, name='stemweb_media_root_url')
+    url(r'^', include('Slipper.slip.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}, name='stemweb_media_root_url')
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

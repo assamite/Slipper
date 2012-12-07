5740d84f8311fe93ed0a3d7985aee5036e9c6f4e
Assamite <slinkola@cs.helsinki.fi>
2012-12-07 15:09:47 +0200

    Umph. that local_conf now

---
+++ local_conf.py
@@ -0,0 +1,10 @@
+DEBUG = False
+
+# tuple of 2-tuples: (name, email)
+ADMINS = (('Simo Linkola', 'slinkola@cs.helsinki.fi'))
+
+# Used for cryptographic thingies
+UGH_DAT_VERRY_SECRAT_KEYZ = 'isnfainfaiwnf'
+
+# This might need some local prefix, depending on apache, etc.
+MEDIA_URL = "/media/"
